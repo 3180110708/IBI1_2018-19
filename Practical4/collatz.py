21abaf3e5891d1c488758ac55540908a853e7ba0
Wang <3180110708@zju.edu.cn>
2019-03-13 09:57:43 +0800

    collatz and some modifications

---
+++ Practical4/collatz.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:43:13 2019
+
+@author: jxm72
+"""
+
+#judge whether n is even or odd
+#if even, multiple 3 and plus 1; if odd, divide by 2
+#when n==1, stop
+n=int(input())
+while n!=1:
+    if n%2==0:
+        n=n/2
+        print(n)
+    elif n%2==1:
+        n=n*3+1
+        print(n)
+    else:
+        print('n is not an integer')
--- Practical4/variables.py
+++ Practical4/variables.py
@@ -9,10 +9,19 @@ a = 923
 b = 923923
 if b % 7 == 0:
     print('b can be devided by 7')
-c = b % 7
-d = c % 11
-e = d % 13
+c = b/7
+d = c/11
+e = d/13
 if e >a:
     print('e is greater')
 else:
     print('a is greater')
+    
+    
+    
+x = True 
+y = False
+z = x and not y or y and not x
+print(x!=y and z==True)
+w = x!=y
+print(w==z)
