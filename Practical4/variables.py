47ab322a6b48876465ecd3b130181166cb8b2252
Wang <3180110708@zju.edu.cn>
2019-03-13 09:32:12 +0800

    variables

---
+++ Practical4/variables.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:21:56 2019
+
+@author: jxm72
+"""
+
+a = 923
+b = 923923
+if b % 7 == 0:
+    print('b can be devided by 7')
+c = b % 7
+d = c % 11
+e = d % 13
+if e >a:
+    print('e is greater')
+else:
+    print('a is greater')
